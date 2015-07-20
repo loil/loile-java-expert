a3551ed82d03f89bceb3ccbab6b40171d35dbcc0
loile <loiletan1990@gmail.com>
2015-07-20 20:30:44 +0700

    Change .xml configuration to Java Configuration

---
+++ src/main/java/com/loile/javaexpert/config/WebSpringConfig.java
@@ -0,0 +1,35 @@
+package com.loile.javaexpert.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.loile.javaexpert.controller")
+public class WebSpringConfig extends WebMvcConfigurerAdapter {
+/*
+	 @Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+	 }
+*/
+	 @Bean
+	 public ViewResolver configureViewResolver() {
+	     InternalResourceViewResolver viewResolve = new InternalResourceViewResolver();
+	     viewResolve.setPrefix("/WEB-INF/views/");
+	     viewResolve.setSuffix(".jsp");
+
+	     return viewResolve;
+	 }
+
+	 @Override
+	 public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+	   configurer.enable();
+	 }
+}
